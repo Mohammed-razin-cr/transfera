343e883c23e51fe266f586f35669082c022b355f
Mohammed Razin <mohammedrazincr786@gmail.com>
2026-06-27 16:58:22 +0530

    commit6: add proxy to vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,17 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/live': 'http://localhost:8080',
+      '/ws': 'http://localhost:8080',
+      '/d': 'http://localhost:8080',
+      '/u': 'http://localhost:8080',
+      '/upload': 'http://localhost:8080',
+      '/health': 'http://localhost:8080',
+      '/static': 'http://localhost:8080',
+    }
+  },
   build: {
     outDir: '../web/assets',
     emptyOutDir: true,
